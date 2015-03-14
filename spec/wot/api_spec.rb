f77964147b3994bf31142ff088e27b4c29b46149
Groza Sergiu <serioja90@gmail.com>
2015-03-14 01:09:12 +0100

    Defined new tests

--- spec/wot/api_spec.rb
+++ spec/wot/api_spec.rb
@@ -1,7 +1,29 @@
 require 'spec_helper'
 
 describe Wot::Api do
+
   it 'has a version number' do
     expect(Wot::Api::VERSION).not_to be nil
+  end
+
+  it 'should create a new api instance for each cluster' do
+    Wot::Api::CLUSTERS.keys.each do |region|
+      expect(Wot::Api.new(region)).not_to be nil
+    end
+  end
+
+  it 'should find a player and all related info' do
+    api          = Wot::Api.new("RU")
+    player       = api.player("serioja90")
+    tanks        = player.tanks
+    achievements = player.achievements
+    info         = player.info
+    stats        = player.stats
+    
+    expect(player).not_to be nil
+    expect(tanks).not_to be nil
+    expect(achievements).not_to be nil
+    expect(info).not_to be nil
+    expect(stats).not_to be nil
   end
 end
