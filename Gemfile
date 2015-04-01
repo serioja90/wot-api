391a58472170cad99e1395df7b36b2c5d9cc5f6c
Groza Sergiu <serioja90@gmail.com>
2015-04-02 00:30:25 +0200

    Refactor rspec tests structure

--- Gemfile
+++ Gemfile
@@ -2,3 +2,11 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in wot-api.gemspec
 gemspec
+
+gem 'httparty'
+
+group :test do
+  gem 'webmock'
+  gem 'vcr'
+  gem 'rake'
+end
--- spec/lib/wot/api_spec.rb
+++ spec/lib/wot/api_spec.rb
@@ -1,6 +1,15 @@
+#!/usr/bin/ruby
+# @Author: sergiu
+# @Date:   2015-04-02 00:13:44
+# @Last Modified by:   sergiu
+# @Last Modified time: 2015-04-02 00:20:48
 require 'spec_helper'
 
 describe Wot::Api do
+
+  before do
+    VCR.insert_cassette 'api', record: :new_episodes
+  end
 
   it 'has a version number' do
     expect(Wot::Api::VERSION).not_to be nil
@@ -37,5 +46,9 @@ describe Wot::Api do
     players = api.players([player.id])
     expect(players).not_to be nil
     expect(players).not_to be_empty
+  end
+
+  after do
+    VCR.eject_cassette
   end
 end
